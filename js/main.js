e4430bb3d81b59c8edaddeb3e485b1b4f138a7a4
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-10-16 11:58:37 +0200

    help

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ oldgreeting.greet()
 
 function basicHaiku()
 {
-    return ["Toward those short trees","We saw a batman descending","On a day in spring."]
+    return ["Ik ben depressief","Jaren al geen vrouw gezien","Ik ben zo alleen"]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
